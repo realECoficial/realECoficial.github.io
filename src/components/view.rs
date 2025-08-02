b5e04501290934f6099d628bf6a0830e86958df6
realec <realec20xx@protonmail.com>
2025-08-02 18:35:30 -0400

    Study: Philosophy: 123, Math: 1(i figure out lexical by my own way but i want it to undertand the whole movie)

--- src/components/view.rs
+++ src/components/view.rs
@@ -160,7 +160,7 @@ pub fn Seccion_estudio_rust() -> Element {
                 } 
             } 
             a {target:"_blank",href: "https://www.susanrigetti.com/philosophy" , 
-                h2 {  "Filosofia: Think (cap 71)" }
+                h2 {  "Filosofia: Think (cap 123)" }
             } 
                 ol {
                     li {a{target:"_blank", href: ESTUDIO1,"Think. A Compelling Introduction To Philosophy"}} 
