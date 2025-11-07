009923a5427e9b1f24dbf6dc9f7c1a329e1e6d5e
realec20xx@protonmail.com <realec20xx@protonmail.com>
2025-11-07 15:08:14 -0300

    mish

--- src/components/view.rs
+++ src/components/view.rs
@@ -166,7 +166,7 @@ pub fn Seccion_estudio_rust() -> Element {
                 } 
             } 
             a {target:"_blank",href: "https://www.susanrigetti.com/philosophy" , 
-                h2 {  "Filosofia: Think (cap 477)" }
+                h2 {  "Filosofia: Think (cap 510)" }
             } 
                 ol {
                     li {a{target:"_blank", href: ESTUDIO1,"Think. A Compelling Introduction To Philosophy"}} 
